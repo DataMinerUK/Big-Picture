438c9a069252b909d445cdd41a9d960c108d788c
David Bello <dhbello@hotmail.com>
2011-09-27 04:44:43 -0500

    First version

---
+++ js/resources.js
@@ -0,0 +1,129 @@
+function init_resources() {
+    loop_update_resources();    
+}
+
+function openURL(urlink ) {
+    $("#tabs").tabs('select', '#tabs-0');
+    $("#mainV")[0].src = urlink;
+}
+
+function update_resources() {
+     $.ajax({
+        url: "http://pipes.yahoo.com/pipes/pipe.run?_id=2c07ef10e470d2641c57d0e492121db5&_render=json&role=" + role + "&room=" + room,
+        dataType: 'json',
+        //url: "http://184.72.38.228/MoJo/Resources.ashx?room=" + room + "&cmd=list&role=" + role,
+        success: function (feed) {
+            var html = "";
+            $(feed.value.items).each(function () {
+                var item = $(this)[0];
+                var currenthtml = "";
+
+                if (item.updated == "") {
+                    currenthtml = "<li class='new' id='" + item.id + "'>" +
+                               "<a class='tweet_avatar' href='#' onclick='javascript:openURL(\"" + item.link + "\");'><img src='./temp/" + item.id + ".jpg' width='128' height='96' border='1'/></a>" +
+                               "<a href='#' onclick='javascript:openURL(\"" + item.link + "\");'>" + item.title + "</a><br />" +
+                               "<span class='tweet_text'>" + item.description + "</span>" +
+                               "<p style='text-align: right;'><input type='button' value='Delete' onclick='javascript:delete_resources(\"" + item.id + "\")' />&nbsp;<input type='button' value='Approve' onclick='javascript:approve_resources(\"" + item.id + "\")' />&nbsp;<input type='button' value='Set in main view' onclick='javascript:main_resources(\"" + item.link + "\")' /></p>" +
+                               "</li>";
+                } else {
+                    if (role == "admin") {
+                        currenthtml = "<li class='new' id='" + item.id + "'>" +
+                               "<a class='tweet_avatar' href='#' onclick='javascript:openURL(\"" + item.link + "\");'><img src='./temp/" + item.id + ".jpg' width='128' height='96' border='1'/></a>" +
+                               "<a href='#' onclick='javascript:openURL(\"" + item.link + "\");'>" + item.title + "</a><br />" +
+                               "<span class='tweet_text'>" + item.description + "</span>" +
+                               "<p style='text-align: right;'><input type='button' value='Delete' onclick='javascript:delete_resources(\"" + item.id + "\")' />&nbsp;<input type='button' value='Set in main view' onclick='javascript:main_resources(\"" + item.link + "\")' /></p>" +
+                               "</li>";
+                    } else {
+                        currenthtml = "<li class='new' id='" + item.id + "'>" +
+                               "<a class='tweet_avatar' href='#' onclick='javascript:openURL(\"" + item.link + "\");'><img src='./temp/" + item.id + ".jpg' width='128' height='96' border='1'/></a>" +
+                               "<a href='#' onclick='javascript:openURL(\"" + item.link + "\");'>" + item.title + "</a><br />" +
+                               "<span class='tweet_text'>" + item.description + "</span>" +
+                               "</li>";
+                    };
+                };
+
+                if ($("#resourceslist").children('#' + item.id).length == 0) {
+                    if (item.title != "") {
+                        html = html + currenthtml;
+                    };
+                } else {
+                    if (item.title == "") {
+                        $("#resourceslist").children('#' + item.id).remove();
+                    } else {
+                        $("#resourceslist").children('#' + item.id).html(currenthtml);
+                    };
+                };
+            });
+
+            $("#resourceslist").append(html);
+            $("#resourceslist").children().removeClass('tweet_even');
+            $("#resourceslist").children().removeClass('tweet_odd');
+            $("#resourceslist").children('li:odd').addClass('tweet_even');
+            $("#resourceslist").children('li:even').addClass('tweet_odd');
+            $("input:button").button();
+        }
+    });
+    
+}
+
+function loop_update_resources() {
+    update_resources();
+    setTimeout("loop_update_resources();", 15000);
+}
+
+function suggest_resource() {
+    $("#title")[0].value = "";
+    $("#description")[0].value = "";
+    $("#urlR")[0].value = "";
+    
+    $("#dialog").dialog({
+        resizable: false,
+        width: 500,
+        buttons: {
+            Ok: function () {
+                $.ajax({
+                    url: "http://184.72.38.228/MoJo/Resources.ashx?room=" + room + "&cmd=add&title=" + $("#title")[0].value + "&description=" + $("#description")[0].value + "&urlR=" + $("#urlR")[0].value + "&role=" + role,
+                    success: function () {
+                        $("#okdialog").dialog({
+                            resizable: false,
+                            buttons: {                                
+                                Ok: function () {
+                                    $(this).dialog("close");                                    
+                                    update_resources();
+                                }
+                            }
+                        });
+                    }
+                });
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+
+function main_resources(urlink){
+    session.signal();
+}
+
+function delete_resources(id) {
+    $.ajax({
+        url: "http://184.72.38.228/MoJo/Resources.ashx?room=" + room + "&cmd=delete&id=" + id + "&role=" + role,
+        success: function () {
+            $("#resourceslist").children('#' + id).remove();
+            update_resources();
+        }
+    });
+}
+
+function approve_resources(id) {
+    $.ajax({
+        url: "http://184.72.38.228/MoJo/Resources.ashx?room=" + room + "&cmd=approve&id=" + id + "&role=" + role,
+        success: function () {
+            $("#resourceslist").children('#' + id).remove();
+            update_resources();
+        }
+    });
+}
---
+++ js/video.js
@@ -0,0 +1,231 @@
+var apiKey = 2578892;
+var sessionId = '2_MX4wfn4yMDExLTA5LTE1IDE0OjQ3OjAxLjgwNzk1NSswMDowMH4wLjM2NjMyODgxNDEyM34';
+var token = 'devtoken';
+
+var session;
+var archive;
+var publisher;
+var subscribers = {};
+var streamsList = new Array();
+var deviceManager;
+var devicePanel;
+var panelShowing = false;
+
+function init_video(){
+    $("#videoStatus").html("Status: Not connected.");
+    show('connectLink');
+    hide('disconnectLink');
+    hide('publishLink');
+    hide('unpublishLink');
+    hide('recordLink');
+    hide('stopLink');
+        
+    if (TB.checkSystemRequirements() != TB.HAS_REQUIREMENTS) {
+        alert('Minimum System Requirements not met!');
+    } else {
+
+        session = TB.initSession(sessionId);
+        session.addEventListener('sessionConnected', sessionConnectedHandler);
+        session.addEventListener('sessionDisconnected', sessionDisconnectedHandler);
+        session.addEventListener('connectionCreated', connectionCreatedHandler);
+        session.addEventListener('connectionDestroyed', connectionDestroyedHandler);
+        session.addEventListener('streamCreated', streamCreatedHandler);
+        session.addEventListener('streamDestroyed', streamDestroyedHandler);
+        session.addEventListener("signalReceived", signalReceivedHandler);
+        connect();
+    };
+
+}
+
+function connect() {
+    session.connect(apiKey, token);
+}
+
+function disconnect() {
+    session.disconnect();
+    hide('recordLink');
+    hide('stopLink');
+    hide('disconnectLink');
+    hide('publishLink');
+    hide('unpublishLink');
+}
+
+// Called when user wants to start publishing to the session
+function startPublishing() {
+    if (!publisher) {
+        var parentDiv = document.getElementById("myCamera");
+        var publisherDiv = document.createElement('div'); // Create a div for the publisher to replace
+        publisherDiv.setAttribute('id', 'opentok_publisher');
+        parentDiv.appendChild(publisherDiv);
+        publisher = session.publish(publisherDiv.id); // Pass the replacement div id to the publish method
+        show('unpublishLink');
+        hide('publishLink');
+    }
+}
+
+function stopPublishing() {
+    if (publisher) {
+        session.unpublish(publisher);
+    }
+    publisher = null;
+
+    show('publishLink');
+    hide('unpublishLink');
+}
+
+function sessionConnectedHandler(event) {
+
+/*    for (var j = 0; j < event.connections.length; j++) {
+        var currenthtml = "<li id='" + event.connections[j].connectionId + "'>" +
+                               event.connections[j].connectionId + "<br />" +
+                               "<p style='text-align: right;'><input type='button' value='Request video' onclick='javascript:request_video(\"" + event.connections[i].connectionId + "\")' />&nbsp;<input type='button' value='Disconnect Video' onclick='javascript:disconnect_video(\"" + event.connections[j].connectionId + "\")' /></p>" +
+                                "</li>";
+        $("#userslist").append(currenthtml);
+
+        $("#userslist").children().removeClass('tweet_even');
+        $("#userslist").children().removeClass('tweet_odd');
+        $("#userslist").children('li:odd').addClass('tweet_even');
+        $("#userslist").children('li:even').addClass('tweet_odd');
+        $("input:button").button();
+    };*/
+
+    // Subscribe to all streams currently in the Session
+    for (var i = 0; i < event.streams.length; i++) {
+        addStream(event.streams[i]);
+    }
+
+    $("#videoStatus").html("Status: Connected.");
+    show('disconnectLink');
+    show('publishLink');
+    show('recordLink');
+    hide('connectLink');
+    hide('unpublishLink');
+}
+
+function streamCreatedHandler(event) {
+    // Subscribe to the newly created streams
+    for (var i = 0; i < event.streams.length; i++) {
+        addStream(event.streams[i]);
+    }
+}
+
+function streamDestroyedHandler(event) {
+    // This signals that a stream was destroyed. Any Subscribers will automatically be removed.
+    // This default behaviour can be prevented using event.preventDefault()
+    /*
+    for (var i = 0; i < event.connections.length; i++) {
+        $("#userslist").children('#' + event.connections[i].connectionId).remove();
+
+        $("#userslist").children().removeClass('tweet_even');
+        $("#userslist").children().removeClass('tweet_odd');
+        $("#userslist").children('li:odd').addClass('tweet_even');
+        $("#userslist").children('li:even').addClass('tweet_odd');
+    };
+    */
+}
+
+function sessionDisconnectedHandler(event) {
+    // This signals that the user was disconnected from the Session. Any subscribers and publishers
+    // will automatically be removed. This default behaviour can be prevented using event.preventDefault()
+    publisher = null;
+
+    $("#videoStatus").html("Status: Not connected.");
+    show('connectLink');
+    hide('disconnectLink');
+    hide('publishLink');
+    hide('unpublishLink');
+    hide('recordLink');
+}
+
+function connectionDestroyedHandler(event) {
+    // This signals that connections were destroyed
+}
+
+function connectionCreatedHandler(event) {
+    // This signals new connections have been created.
+}
+
+/*
+If you un-comment the call to TB.addEventListener("exception", exceptionHandler) above, OpenTok calls the
+exceptionHandler() method when exception events occur. You can modify this method to further process exception events.
+If you un-comment the call to TB.setLogLevel(), above, OpenTok automatically displays exception event messages.
+*/
+function exceptionHandler(event) {
+    alert("Exception: " + event.code + "::" + event.message);
+}
+
+
+function addStream(stream) {
+    // Check if this is the stream that I am publishing, and if so do not publish.
+    if (stream.connection.connectionId == session.connection.connectionId) {
+        //return;
+    }
+    var subscriberDiv = document.createElement('div'); // Create a div for the subscriber to replace
+    //subscriberDiv.style.MozTransform = 'rotate(15deg)';
+    subscriberDiv.setAttribute('id', stream.streamId); // Give the replacement div the id of the stream as its id.
+    document.getElementById("subscribers").appendChild(subscriberDiv);
+    subscribers[stream.streamId] = session.subscribe(stream, subscriberDiv.id);
+    streamsList.push(stream);
+}
+
+function show(id) {
+    $('#' + id).show();
+}
+
+function hide(id) {
+    $('#' + id).hide();
+}
+
+function signal() {
+    session.signal();
+}
+
+function toggleDevicePanel() {
+    if (panelShowing) {
+        if (devicePanel) {
+            deviceManager.removePanel(devicePanel);
+        }
+        document.getElementById("toggleDevicePanelLink").value = 'Camera Settings';
+        hide("devicePanelContainer");
+        panelShowing = false;
+    } else {
+        var newdiv = document.createElement("div");
+        newdiv.id = "devicePanel";
+        document.getElementById("devicePanelInset").appendChild(newdiv);
+        if (deviceManager == null) {
+            deviceManager = TB.initDeviceManager(apiKey);
+        }
+        devicePanel = deviceManager.displayPanel("devicePanel", publisher, { "showCloseButton": false });
+        document.getElementById("toggleDevicePanelLink").value = 'Hide Camera Settings';
+        show("devicePanelContainer");
+        panelShowing = true;
+    }
+}
+
+function stop_video() {
+    session.stopRecording(archive);
+    $('#titleH').html("Big Picture: " + archive.archiveId);
+    archive = null;
+
+    show('recordLink');
+    hide('stopLink');
+}
+
+function record_video() {
+    hide('recordLink');
+    show('stopLink');
+    archive = session.createArchive(apiKey, "perSession", "BigPicture " + date.now);
+    session.startRecording(archive); 
+}
+
+function request_video(id) {
+
+}
+
+function disconnect_video(id) {
+    for (var i = 0; i < streamsList.length; i++) {
+        if (streamsList[i].connection.connectionId == id) {
+            session.forceUnpublish(streamsList[i]);
+        };
+    };
+}
